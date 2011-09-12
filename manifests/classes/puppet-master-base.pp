43a505d03b929b746fb8ef1c69362954688b6f01
Marc Fournier <marc.fournier@camptocamp.com>
2011-09-12 15:06:08 +0200

    puppet::master - split mysql resources in dedicated class.

--- manifests/classes/puppet-master-base.pp
+++ manifests/classes/puppet-master-base.pp
@@ -1,5 +1,4 @@
 class puppet::master::base {
-  include mysql::server
 
   package { "puppetmaster":
     ensure => present,
@@ -12,27 +11,59 @@ class puppet::master::base {
   # used by puppetdoc -m pdf
   package { "python-docutils": ensure => present }
 
-  package { "ruby-mysql":
-    ensure => present,
-    name   => $operatingsystem ? {
-      /Debian|Ubuntu/        => "libdbd-mysql-ruby",
-      /RedHat|CentOS|Fedora/ => "ruby-mysql",
-    },
-  }
-
   if $operatingsystem =~ /RedHat|CentOS|Fedora/ {
     package { "ruby-rdoc": ensure => present }
   }
 
-  # Database
-  mysql::database { "puppet":
-    ensure => present,
+  if (versioncmp($puppetversion, 2) > 0) {
+    $master = "master"
+  } else {
+    $master = "puppetmasterd"
   }
 
-  mysql::rights { "Set rights for puppet database":
-    ensure   => present,
-    database => "puppet",
-    user     => "puppet",
-    password => "puppet"
+  case $puppetdbtype {
+    "mysql": {
+
+      package { "ruby-mysql":
+        ensure => present,
+        name   => $operatingsystem ? {
+          /Debian|Ubuntu/        => "libdbd-mysql-ruby",
+          /RedHat|CentOS|Fedora/ => "ruby-mysql",
+        },
+      }
+
+      puppet::config {
+        "${master}/dbadapter":    value => "mysql";
+        "${master}/storeconfigs": value => "true";
+      }
+    }
+
+    "sqlite": {
+      package { ["sqlite3", "libsqlite3-ruby"]:
+        ensure => present,
+      }
+
+      puppet::config {
+        "${master}/dbadapter":    value => "sqlite3";
+        "${master}/storeconfigs": value => "true";
+      }
+    }
+
+    default: {
+      puppet::config {
+        "${master}/dbadapter":    ;
+        "${master}/storeconfigs": value => "false";
+      }
+    }
+
   }
+
+  puppet::config {
+    "${master}/dbmigrate":  value => "true";
+    "${master}/dbserver":   value => $puppetdbhost;
+    "${master}/dbname":     value => $puppetdbname;
+    "${master}/dbuser":     value => $puppetdbuser;
+    "${master}/dbpassword": value => $puppetdbpw;
+  }
+
 }
