1394e0dc956e4f6dbb153f62b505de35138f7ce3
Raphaël Pinson <raphink@gmail.com>
2012-07-19 10:59:37 +0200

    Add tests for puppet::environment

---
+++ spec/defines/puppet_environment_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'puppet::environment' do
+  describe 'When present' do
+    let(:title) { 'foo' }
+    let(:params) { { :path => '/home/foo' } }
+    it { should contain_puppet__config('foo/modulepath').with_value('/home/foo/puppetmaster/modules:/home/foo/puppetmaster/site-modules') }
+    it { should contain_puppet__config('foo/manifestdir').with_value('/home/foo/puppetmaster/manifests') }
+    it { should contain_puppet__config('foo/manifest').with_value('/home/foo/puppetmaster/manifests/site.pp') }
+  end
+
+  describe 'When absent' do
+    let(:title) { 'foo' }
+    let(:params) { { :path => '/home/foo', :ensure => 'absent' } }
+    it { should contain_augeas('remove environment foo').with_changes('rm /files/etc/puppet/puppet.conf/foo') }
+  end
+end
