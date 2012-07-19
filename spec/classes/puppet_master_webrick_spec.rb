93ca95b1ddbe229162858ba4e350fd7963ad3f1e
Raphaël Pinson <raphink@gmail.com>
2012-07-19 15:20:33 +0200

    Add spec for puppet::master::webrick

---
+++ spec/classes/puppet_master_webrick_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'puppet::master::webrick' do
+  describe 'When on Debian' do
+    let(:facts) { {
+      :operatingsystem => 'Debian'
+    } }
+
+    it do should contain_augeas('configure puppetmaster startup variables').with(
+      'context' => '/files/etc/default/puppetmaster',
+      'changes' => [
+        'set PORT 8140',
+        'set START yes',
+        'set SERVERTYPE webrick',
+        'set PUPPETMASTERS 1'
+    ]) end
+
+    it do should contain_service('puppetmaster').with(
+      'ensure' => 'running',
+      'enable' => 'true'
+    ) end
+  end
+
+  describe 'When on RedHat' do
+    let(:facts) { {
+      :operatingsystem => 'RedHat'
+    } }
+
+    it do should contain_augeas('configure puppetmaster startup variables').with(
+      'context' => '/files/etc/sysconfig/puppetmaster',
+      'changes' => [
+        'set PUPPETMASTER_EXTRA_OPTS "--servertype=webrick"',
+        'rm  PUPPETMASTER_PORTS'
+    ]) end
+
+    it do should contain_service('puppetmaster').with(
+      'ensure' => 'running',
+      'enable' => 'true'
+    ) end
+  end
+end
