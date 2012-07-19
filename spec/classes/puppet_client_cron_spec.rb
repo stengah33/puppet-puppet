1a0c9c59c4f181845afed0076fcb1090c12416de
Raphaël Pinson <raphink@gmail.com>
2012-07-19 12:14:20 +0200

    Add tests for puppet::client::cron

---
+++ spec/classes/puppet_client_cron_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'puppet::client::cron' do
+  describe 'When on Debian' do
+    let(:facts) { {
+      :operatingsystem    => 'Debian',
+      :ipaddress          => '10.0.0.1',
+    } }
+
+    it do should contain_service('puppet').with(
+      'ensure'    => 'stopped',
+      'enable'    => 'false',
+      'hasstatus' => 'false',
+      'pattern'   => 'ruby /usr/sbin/puppetd -w 0'
+    ) end
+
+    it do should contain_file('/usr/local/bin/launch-puppet').with(
+      'ensure'  => 'present',
+      'mode'    => '0755'
+    ) end
+
+    id do should contain_cron('puppetd').with(
+      'ensure'      => 'present',
+      'command'     => '/usr/local/bin/launch-puppet',
+      'user'        => 'root',
+      'environment' => 'MAILTO=root',
+      'minute'      => 'unknown'
+    ) end
+  end
+end
