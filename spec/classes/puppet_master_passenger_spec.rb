070ece1d586be9c35af055a77855f5f9c4c15ce7
Raphaël Pinson <raphink@gmail.com>
2012-07-19 16:20:43 +0200

    Test puppet::master::passenger with variables

--- spec/classes/puppet_master_passenger_spec.rb
+++ spec/classes/puppet_master_passenger_spec.rb
@@ -69,4 +69,67 @@ class ruby::passenger::apache {}
       'provider' => 'gem'
     ) end
   end
+
+  describe 'When setting variables on RedHat' do
+    let(:facts) { {
+      :operatingsystem      => 'RedHat',
+      :puppetmaster_ssldir  => '/etc/ssl/puppet',
+      :puppet_server        => 'pm.example.com',
+      :wwwroot              => '/var/www',
+      :rack_version         => '1.2.3',
+      :activerecord_version => '3.2.1',
+      :puppetmaster_timeout => '314'
+    } }
+
+    it { should include_class('apache::params') }
+    it { should include_class('ruby::gems') }
+    it { should include_class('ruby::passenger::apache') }
+
+    it { should contain_apache__module('passenger') }
+
+    it do should contain_apache__vhost-ssl('puppetmasterd').with(
+      'mode' => '2755',
+      'user' => 'root'
+    ) end
+
+    it { should contain_apache__listen('8140') }
+
+    it do should contain_file('/var/www/puppetmasterd/htdocs/rack').with(
+      'ensure' => 'directory',
+      'mode'   => '0755',
+      'owner'  => 'root',
+      'group'  => 'root'
+    ) end
+
+    it do should contain_file('/var/www/puppetmasterd/htdocs/rack/public').with(
+      'ensure' => 'directory',
+      'mode'   => '0755',
+      'owner'  => 'root',
+      'group'  => 'root'
+    ) end
+
+    it do should contain_file('/var/www/puppetmasterd/htdocs/rack/tmp').with(
+      'ensure' => 'directory',
+      'mode'   => '0755',
+      'owner'  => 'root',
+      'group'  => 'root'
+    ) end
+
+    it do should contain_file('/var/www/puppetmasterd/htdocs/rack/config.ru').with(
+      'ensure' => 'present',
+      'mode'   => '0644',
+      'owner'  => 'puppet',
+      'group'  => 'root'
+    ) end
+
+    it do should contain_package('activerecord').with(
+      'ensure'   => '3.2.1',
+      'provider' => 'gem'
+    ) end
+
+    it do should contain_package('rack').with(
+      'ensure'   => '1.2.3',
+      'provider' => 'gem'
+    ) end
+  end
 end
