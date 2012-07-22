a661a7224e38b0263872ce91b9b41df925059282
Raphaël Pinson <raphink@gmail.com>
2012-07-22 16:24:58 +0200

    Loop on oses in puppet_master_mongrel_standalone_spec.rb

--- spec/classes/puppet_master_mongrel_standalone_spec.rb
+++ spec/classes/puppet_master_mongrel_standalone_spec.rb
@@ -1,56 +1,27 @@
 require 'spec_helper'
 
 describe 'puppet::master::mongrel::standalone' do
-  describe 'When on Debian' do
+  OSES.each do |os|
+    describe "When on #{os} with mysql" do
     let(:facts) { {
-      :operatingsystem => 'Debian'
+      :operatingsystem => os
     } }
 
-    it do should contain_package('mongrel').with(
-      'ensure' => 'present',
-      'name'   => 'mongrel'
-    ) end
+      it do should contain_package('mongrel').with(
+        'ensure' => 'present',
+        'name'   => VARS[os]['mongrel']
+      ) end
 
-    it do should contain_service('puppetmaster').with(
-      'ensure'    => 'running',
-      'hasstatus' => 'true',
-      'enable'    => 'true'
-    ) end
+      it do should contain_service('puppetmaster').with(
+        'ensure'    => 'running',
+        'hasstatus' => 'true',
+        'enable'    => 'true'
+      ) end
 
-    it do should contain_augeas('configure puppetmaster startup variables').with(
-      'context' => '/files/etc/default/puppetmaster',
-      'changes' => [
-        'set PORT 18140',
-        'set START yes',
-        'set SERVERTYPE mongrel',
-        'set PUPPETMASTERS 4'
-    ]) end
-  end
-
-  describe 'When on RedHat' do
-    let(:facts) { {
-      :operatingsystem => 'RedHat'
-    } }
-
-    it do should contain_package('mongrel').with(
-      'ensure' => 'present',
-      'name'   => 'rubygem-mongrel'
-    ) end
-
-    it do should contain_service('puppetmaster').with(
-      'ensure'    => 'running',
-      'hasstatus' => 'true',
-      'enable'    => 'true'
-    ) end
-
-    it do should contain_augeas('configure puppetmaster startup variables').with(
-      'context' => '/files/etc/sysconfig/puppetmaster',
-      'changes' => [
-        'set PUPPETMASTER_EXTRA_OPTS \'"--servertype=mongrel"\'',
-        'set PUPPETMASTER_PORTS/1 18140',
-        'set PUPPETMASTER_PORTS/2 18141',
-        'set PUPPETMASTER_PORTS/3 18142',
-        'set PUPPETMASTER_PORTS/4 18143'
-    ]) end
+      it do should contain_augeas('configure puppetmaster startup variables').with(
+        'context' => "/files#{puppetmaster_default}",
+        'changes' => VARS[os]['mongrel_settings']
+      ) end
+    end
   end
 end
--- spec/spec_params.rb
+++ spec/spec_params.rb
@@ -7,16 +7,33 @@ require 'puppet'
 PUPPETVERSION = Puppet::PUPPETVERSION.to_s
 
 VARS = {
-  'Debian'             => {
-    'puppetmaster_pkg' => 'puppetmaster',
-    'lsb_release'      => 'lsb-release',
-    'ruby_mysql'       => 'libdbd-mysql-ruby',
+  'Debian'                 => {
+    'puppetmaster_pkg'     => 'puppetmaster',
+    'lsb_release'          => 'lsb-release',
+    'ruby_mysql'           => 'libdbd-mysql-ruby',
+    'mongrel'              => 'mongrel',
+    'puppetmaster_default' => '/etc/default/puppetmaster',
+    'mongrel_settings'     => [
+      'set PORT 18140',
+      'set START yes',
+      'set SERVERTYPE mongrel',
+      'set PUPPETMASTERS 4'
+    ],
   },
 
   'RedHat' => {
-    'puppetmaster_pkg' => 'puppet-server',
-    'lsb_release'      => 'redhat-lsb',
-    'ruby_mysql'       => 'ruby-mysql',
+    'puppetmaster_pkg'     => 'puppet-server',
+    'lsb_release'          => 'redhat-lsb',
+    'ruby_mysql'           => 'ruby-mysql',
+    'mongrel'              => 'rubygem-mongrel',
+    'puppetmaster_default' => '/etc/sysconfig/puppetmaster',
+    'mongrel_settings'     => [
+      'set PUPPETMASTER_EXTRA_OPTS \'"--servertype=mongrel"\'',
+      'set PUPPETMASTER_PORTS/1 18140',
+      'set PUPPETMASTER_PORTS/2 18141',
+      'set PUPPETMASTER_PORTS/3 18142',
+      'set PUPPETMASTER_PORTS/4 18143',
+    ],
   },
 }
 VARS['Ubuntu'] = VARS['Debian']
